e337985589606a55d8b212c8aeff1cd1d3c99a66
lessex <le160516@ohio.edu>
2021-12-04 14:21:32 -0500

    organize files

--- gui.py
+++ gui.py
@@ -3,6 +3,9 @@ import pandas as pd
 from pandastable import Table
 from main import df
 import matplotlib.pyplot as plt
+from main import barGraphData
+from main import scatterGraphData
+from main import printDataTerminal
 
 
 window = tk.Tk()
@@ -13,56 +16,6 @@ frame.pack()
 window.title('NFL Data')
 bottomFrame = tk.Frame(window)
 bottomFrame.pack(side="bottom")
-
-
-def printDataTerminal():
-    print(df)
-
-
-def scatterGraphData():
-    print('Enter X-axis Column: ')
-    print('Choices:', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD')
-    userInputX = input()
-    if userInputX not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
-        print('Incorrect axis value. ')
-    graphX = df[userInputX]
-    print('Enter Y-axis Column: ')
-    userInputY = input()
-    if userInputY not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
-        print('Incorrect axis value. ')
-    graphY = df[userInputY]
-    plt.xlabel(userInputX)
-    plt.ylabel(userInputY)
-    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
-    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
-    ax = plt.gca()  # get current axes
-    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
-    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
-    plt.scatter(graphX, graphY)
-    plt.show()
-
-
-def barGraphData():
-    print('Enter X-axis Column: ')
-    print('Choices:', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD')
-    userInputX = input()
-    if userInputX not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
-        print('Incorrect axis value. ')
-    graphX = df[userInputX]
-    print('Enter Y-axis Column: ')
-    userInputY = input()
-    if userInputY not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Rate', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
-        print('Incorrect axis value. ')
-    graphY = df[userInputY]
-    plt.xlabel(userInputX)
-    plt.ylabel(userInputY)
-    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
-    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
-    ax = plt.gca()  # get current axes
-    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
-    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
-    plt.bar(graphX, graphY)
-    plt.show()
 
 
 button1 = tk.Button(
--- main.py
+++ main.py
@@ -7,12 +7,58 @@ from pandastable import Table
 df = pd.read_csv('2021offense.csv', sep=r'\s*,\s*', engine='python')
 df = df.drop(columns='"Rk')
 df = df.drop(columns='EXP"')
+df = df.drop(columns='Rate')
+
+def printDataTerminal():
+    print(df)
 
 
-ax = plt.gca()  # get current axes
+def scatterGraphData():
+    print('Enter X-axis Column: ')
+    print('Choices:', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD')
+    userInputX = input()
+    if userInputX not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
+        print('Incorrect axis value. ')
+    graphX = df[userInputX]
+    print('Enter Y-axis Column: ')
+    userInputY = input()
+    if userInputY not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
+        print('Incorrect axis value. ')
+    graphY = df[userInputY]
+    plt.xlabel(userInputX)
+    plt.ylabel(userInputY)
+    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
+    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
+    ax = plt.gca()  # get current axes
+    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
+    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
+    plt.scatter(graphX, graphY)
+    plt.show()
 
-ax.set_xlim([0, 500])  # set x-axis limits to 150, 400
-ax.set_ylim([0, 50])  # set y-axis limits to 0, 14
+
+def barGraphData():
+    print('Enter X-axis Column: ')
+    print('Choices:', 'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD')
+    userInputX = input()
+    if userInputX not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
+        print('Incorrect axis value. ')
+    graphX = df[userInputX]
+    print('Enter Y-axis Column: ')
+    userInputY = input()
+    if userInputY not in {'Cmp', 'Att', 'Cmp%', 'Yds', 'TD', 'TD%', 'Int', 'Int%', 'Lng', 'Y/A', 'AY/A', 'Y/C', 'Y/G', 'Sk', 'Yds', 'NY/A', 'ANY/A', 'Sk%', '4QC', 'GWD'}:
+        print('Incorrect axis value. ')
+    graphY = df[userInputY]
+    plt.xlabel(userInputX)
+    plt.ylabel(userInputY)
+    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
+    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
+    ax = plt.gca()  # get current axes
+    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
+    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
+    plt.bar(graphX, graphY)
+    plt.show()
+
+
 
 
 
