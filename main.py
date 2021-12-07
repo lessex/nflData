950bbacaecdf2b5b5201fb5ed64cd2bdf898f272
lessex <le160516@ohio.edu>
2021-12-07 18:06:31 -0500

    add labels to bar graph

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def printDataTerminal():
 
 def queryDB():
     from gui import queryEntry
-    userInputQuery = queryEntry.get()
-    queryDf = df.query(userInputQuery)
-    print(queryDf)
+    userInputQuery = queryEntry.get() #gathers user's string input to queryEntry box
+    queryDf = df.query(userInputQuery) #create a new df based on query
+    print(queryDf) #output query to terminal
 
 
 def scatterGraphData():
@@ -29,13 +29,17 @@ def scatterGraphData():
     plt.figure(str(userInputX) + ' per ' + str(userInputY))
     plt.xlabel(userInputX)
     plt.ylabel(userInputY)
-    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
-    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
+    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.3 #last df value + 10% of itself
+    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.3 #last df value + 10% of itself
+    firstEntryX = graphX.min()  #gathers lowest x-axis entry as first point
+    firstEntryY = graphY.min()  #gathers lowest y-axis entry as first point
+
+
     ax = plt.gca()  # get current axes
-    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
-    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
+    ax.set_xlim([firstEntryX, lastEntryX])  # set x-axis limits to 150, 400
+    ax.set_ylim([firstEntryY, lastEntryY])  # set y-axis limits to 0, 14
     for i in range (len(graphX)):   #labels scatterplot points with each team name
-        plt.text(x = graphX[i] + 0.2, y = graphY[i] + 0.1, s = df.Tm[i])
+        plt.text(x = graphX[i] + 0.1, y = graphY[i] + 0.1, s = df.Tm[i], fontdict=dict(color = 'black', size = 10), bbox = dict(facecolor = 'yellow', alpha = 0.5))
     plt.scatter(graphX, graphY, cmap= 'viridis')
     plt.colorbar()
     plt.show()
@@ -54,10 +58,14 @@ def barGraphData():
     plt.figure(str(userInputX) + ' per ' + str(userInputY))
     plt.xlabel(userInputX)
     plt.ylabel(userInputY)
-    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.1 #last df value + 10% of itself
-    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.1 #last df value + 10% of itself
+    lastEntryX = graphX.iloc[0] + graphX.iloc[0] * 0.3 #last df value + 10% of itself
+    lastEntryY = graphY.iloc[0] + graphY.iloc[0] * 0.3 #last df value + 10% of itself
+    firstEntryX = graphX.min()  #gathers lowest x-axis entry as first point
+    firstEntryY = graphY.min()  #gathers lowest y-axis entry as first point
     ax = plt.gca()  # get current axes
-    ax.set_xlim([0, lastEntryX])  # set x-axis limits to 150, 400
-    ax.set_ylim([0, lastEntryY])  # set y-axis limits to 0, 14
+    ax.set_xlim([firstEntryX, lastEntryX])  # set x-axis limits lowest x-axis entry
+    ax.set_ylim([firstEntryY, lastEntryY])  # set y-axis limits to lowest y-axis entry
+    for i in range (len(graphX)):   #labels scatterplot points with each team name
+        plt.text(x = graphX[i], y = graphY[i], s = df.Tm[i], fontdict=dict(color = 'black', size = 10), bbox = dict(facecolor = 'yellow', alpha = 0.5), rotation = 90)
     plt.bar(graphX, graphY)
     plt.show()
